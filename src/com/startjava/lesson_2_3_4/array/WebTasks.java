67ba5ad8540cffa167388681668a54a081f36b24
Vacant-name <startjava.com>
2020-05-10 21:33:38 +0300

    homeCommit

--- src/com/startjava/lesson_2_3_4/array/WebTasks.java
+++ src/com/startjava/lesson_2_3_4/array/WebTasks.java
@@ -2,8 +2,7 @@ package com.startjava.lesson_2_3_4.array;
 
 public class WebTasks {
     public static void main(String[] args) {
-        // The task 1. "Find the sum of the positive even elements of an array wich has the even and odd numbers"
-        int[] array = int {-5, -1, 0, 1, 2, 4};
-        System.out.println(array);
+        int[] array = {1, 2, 4, -1, 0, -6};
+        System.out.println(array[5]);
     }
 }
