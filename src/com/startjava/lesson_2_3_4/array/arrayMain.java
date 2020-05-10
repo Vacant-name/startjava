8cbefa2149a6d6147faa817c938cf85ddf4c4000
Vacant-name <startjava.com>
2020-05-10 18:52:27 +0300

    iPhoneCommit

--- src/com/startjava/lesson_2_3_4/array/arrayMain.java
+++ src/com/startjava/lesson_2_3_4/array/arrayMain.java
@@ -3,18 +3,28 @@ package com.startjava.lesson_2_3_4.array;
 public class arrayMain {
 
     public static void main(String[] args) {
-
         int[] numbers = new int[6];
-
         float[] numbers1 = {3.4f, .5f, 200.3f};
 
 //        System.out.println(numbers.length);
-//        System.out.println(numbers1.length);
-
+//        System.out.println(numbers1.length)
 //        System.out.println(numbers);
 
+        for (float number : numbers1) {
+            System.out.print(number + " ");
+        }
+
         for(int i = 0; i < numbers1.length; i++) {
-            System.out.print(numbers1[i] + " ");
+            numbers[i] = i * 10;
+        }
+
+        for (float number : numbers1) {
+            System.out.print(number + " ");
+        }
+        String[] text = new String[3];
+
+        for(String string : text) {
+            System.out.println(string);
         }
     }
 }
---
+++ src/com/startjava/lesson_2_3_4/array/arrayMaxElementMain.java
@@ -0,0 +1,27 @@
+package com.startjava.lesson_2_3_4.array;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class arrayMaxElementMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Set the lenght of the array: ");
+        int maxLenght = sc.nextInt();
+        int[] numbers = new int[maxLenght];
+
+        System.out.println("Fill the array with integers separated by a space: ");
+        for(int i = 0; i < maxLenght; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
+        int max = numbers[0];
+        for(int i = 0; i < maxLenght - 1; i++) {
+            if(max < numbers[i + 1]) {
+                max = numbers[i + 1];
+            }
+            System.out.println("MAX number = " + max);
+        }
+    }
+}
